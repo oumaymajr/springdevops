ad5c219aaf1da57f5d5d3f4e0f824bc8283a2951
2nour <you@example.com>
2022-10-28 20:46:41 +0100

    sonarqubepipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,6 +16,25 @@ pipeline {
                     echo date
                 }
             }
+
+            stage('MVN CLEAN') {
+                steps{
+                    sh 'mvn clean'
+                }
+            }
+
+            stage('MVN COMPILE') {
+                steps{
+                    sh 'mvn compile'
+                }
+            }
+
+            stage('MVN SONARQUBE') {
+                steps{
+                    sh 'mvn sonar:sonar -Dsonar.login=admin -Dsonar.password=ApaMkvk8urqBe9q'
+                }
+            }
+
     }
 
 
