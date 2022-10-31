6244d70d77d3b15f1378dc1760b5ad1e95377f17
oumayma <oumayma.jrayed@esprit.tn>
2022-10-31 10:21:06 +0100

    Update StockTest

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -4,11 +4,13 @@ package com.esprit.examen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @EnableScheduling
 @SpringBootApplication
 public class TpAchatProjectApplication {
+	//@CrossOrigin("/http://localhost:4200") --> angular url
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpAchatProjectApplication.class, args);
