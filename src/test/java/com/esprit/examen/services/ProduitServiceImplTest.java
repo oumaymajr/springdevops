02c8e45776281b6db1f07e26dabc6018d5588fd2
Nour Hadrich <52534067+2nour@users.noreply.github.com>
2022-11-01 00:14:38 +0100

    Update ProduitServiceImplTest.java

--- src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -36,7 +36,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@SpringBootTest
+@SpringBootTest(classes = ProduitServiceImplTest.class)
 
 @Slf4j
 class ProduitServiceImplTest {
