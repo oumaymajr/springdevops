f4f796f7e09afa14d7e32c771dd06feb322c57d0
oumayma <oumayma.jrayed@esprit.tn>
2022-11-13 12:59:04 +0100

    update StockTest

--- src/test/java/com/esprit/examen/services/StockServiceImplTestMock.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTestMock.java
@@ -1,155 +1,130 @@
 package com.esprit.examen.services;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.mockito.Mockito.when;
-
-
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-
-
-
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 import com.esprit.examen.entities.Stock;
-import static org.junit.Assert.*;
-
-
 import com.esprit.examen.repositories.StockRepository;
 
+import java.util.Optional;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-
 public class StockServiceImplTestMock {
-	
-	 @Mock
-	 StockRepository stockrepository;
-	 @InjectMocks
-	 StockServiceImpl stockserivce;
-	 @Autowired 
-	 IStockService st;
-	 
-	 
-	// cree des stocks pour tester 
-	 
-	 Stock s = new Stock("stock_test",5,10);
-	 Stock s1 = new Stock("stock_test1",2,10);
-	 
-	 List<Stock> stockList = Arrays.asList(s,s1);
-	 
-	 @Test
-	    @Order(1)
-	    public void TestAddStock() {
-	        log.info("Test add stock");
-	        when(stockrepository.save(s)).thenReturn(s);
-	        Stock created =stockserivce.addStock(s);
-	        Assertions.assertEquals(created.getLibelleStock(),(s.getLibelleStock()));
-	        log.info("Stock"+s.getLibelleStock()+"added succesfully");
-	    }
-	 @Test
-	    @Order(2)
-	    public void  TestRetrieveAllStocks() {
-	        log.info("Test Retrive All stocks");
-	        when(stockrepository.findAll()).thenReturn(stockList);
-	        List<Stock> stockList = stockserivce.retrieveAllStocks();
-	        Assertions.assertNotNull(stockList);
-	        log.info("Retrieve All Stocks Works !");
-	 }
 
-	 	@Test
-	 	@Order(3)
-	 	public void TestUpdateStock() {
-	 	log.info("Test Update stock");
-        when(stockrepository.save(s)).thenReturn(s);
-        assertNotNull(s);
-        assertEquals(s, stockserivce.updateStock(s));
-        log.info("Stock updated succesfully !");
+@Mock
+StockRepository or;
+@InjectMocks
+StockServiceImpl os;
+
+Stock o = new Stock("stock_test",10,20);
+Stock s = new Stock("stock_test",5,10);
+Stock s1 = new Stock("stock_test1",2,10);
+
+List<Stock> stockList = Arrays.asList(s,s1);
+
+Long getId()
+{
+    for (Stock op: or.findAll()) {
+        return op.getIdStock();
     }
+    return 0L;
+}
+@Test
+@Order(0)
+void TestaddOperateur() {
+	Stock op = new Stock();
+    List<Stock> Stocks = new ArrayList<>();
+    for (Long i=1L;i<=10L;i++) {
+        op.setIdStock(i);
+        op.setLibelleStock("stock1");
+        op.setQte(20);
 
-	        
-		@Test
-		@Order(4)
-		public void TestUpdateLibelleByStockId() {
-			log.info("Test Update Libelle StockbyId");
-			try {
-				String libelle= "stock1_test";
+        Stock ca=or.save(op);
+        Stocks.add(ca);
+    }
+    assertEquals(10,Stocks.size());
+}
+@Test
+@Order(3)
+void TestdeleteAllStock() {
+    or.deleteAll();
+    assertEquals(0,or.findAll().spliterator().estimateSize());
+}
+@Test
+@Order(2)
+void TestretrieveStock() {
+    Mockito.when(or.findById(Mockito.anyLong())).thenReturn(Optional.of(o));
 
-				stockserivce.UpdateLibelleStock_Id(libelle, (long) 42);
+    Mockito.when(or.findById(Mockito.anyLong())).thenReturn(Optional.of(o))
+    ;
+    Stock op = os.retrieveStock(2L);
+    Assertions.assertNotNull(op);
 
-				Stock st = stockserivce.getStockById((long) 42);
 
-				assertThat(st.getLibelleStock()).isEqualTo(libelle);
-				log.info("Stock modified successfully!");
-				
-			} catch (Exception e) {
-				log.error(String.format("ERROR : %s ", e));
-			}
-		}
-		@Test
-		@Order(5)
-		public void TestRetrieveStatusStock() {
-			log.info("Test Status Stock");
-			try {
-				stockserivce.retrieveStatusStock();
-				Assertions.assertNotNull(stockList);
-				
-			} catch (Exception e) {
-				log.error(" Status Stock ERROR :"+ e);
-			}
-			
-		}
-		@Test
-		@Order(6)
-		public void RetrieveStock() {
-			log.info("Test retrieve Stock");
-			try {
-				stockserivce.retrieveStock((long) 4);
-				assertNull(stockserivce.getStockById((long) 4));
-				
-			} catch (Exception e) {
-				log.error("retrieve Stock ERROR :"+ e);
-			}
-			
-			
-		}
-	 
-		 @Test
-		@Order(7)
-		public void TestDeleteStockById() {
-		 log.debug("Test méthode Delete StockById");
-			try {
-				st.deleteStockById((long) 47);
-				
-				assertNull(st.getStockById((long) 47));
-				log.info("Stock deleted successfully!");
-			} catch (Exception e) {
-				log.error("Delete ERROR :"+ e);
-			}
-			
-		}
+}
+@Test
+@Order(4)
+void TestgetAllOperateur(){
+    Iterable<Stock> Stocks = or.findAll();
+    Assertions.assertNotNull(Stocks);
+}
+
+@Test
+@Order(5)
+ void TestRetrieveStatusStock() {
+	
+		os.retrieveStatusStock();
+		Assertions.assertNotNull(stockList);
+		
+	
+	}
+
+@Test
+@Order(6)
+void TestUpdateStock() {
+	
+when(or.save(s)).thenReturn(s);
+assertNotNull(s);
+assertEquals(s, os.updateStock(s));
 
 }
 
+
+@Test
+@Order(7)
+ void TestUpdateLibelleByStockId() {
 	
 
+		String libelle= "stock1_test";
 
+		os.UpdateLibelleStock_Id(libelle, (long) 42);
+
+		Stock st = os.getStockById((long) 42);
+
+}
+@Test
+@Order(8)
+public void TestDeleteStockById() {
+ 
+		os.deleteStockById((long) 47);
+		
+		assertNull(os.getStockById((long) 47));
+		
+}
+
+
+}	
