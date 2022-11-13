b483e991cec0497c630953f4e128693b8fb7ed27
issaoui boukhris <issaoui.boukhris@esprit.tn>
2022-11-13 12:42:47 +0100

    mocktest

--- src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -1,117 +1,77 @@
 package com.esprit.examen.services;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.esprit.examen.entities.Operateur;
 import com.esprit.examen.repositories.OperateurRepository;
-import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-
-
 public class OperateurServiceImplTest {
-private static final Logger l = LogManager.getLogger(OperateurServiceImplTest.class);
 
 @Mock
-private OperateurRepository or;
+OperateurRepository or;
 @InjectMocks
-private OperateurServiceImpl os;
+OperateurServiceImpl os;
 
-@Autowired 
-IOperateurService operateurservice	;
+Operateur o = new Operateur("afandich","issa","****");
 
-Operateur o1 = new Operateur("bkh", "Issa","***");
-Operateur o2 = new Operateur("abc", "Oum","***");
-List<Operateur> operateurlist = Arrays.asList(o1,o2);
+Long getId()
+{
+    for (Operateur op: or.findAll()) {
+        return op.getIdOperateur();
+    }
+    return 0L;
+}
 @Test
-@Order(1)
-public void TestAddOperateur () {
-	Mockito.when(or.save(o1)).thenReturn(o1);
-    assertNotNull(o1);
-    assertEquals(o1,os.addOperateur(o1));
-	System.out.print("operateur "+ o1.getPrenom()+ " added succesfully");
-	}
+@Order(0)
+void TestaddOperateur() {
+	Operateur op = new Operateur();
+    List<Operateur> Operateurs = new ArrayList<>();
+    for (Long i=1L;i<=10L;i++) {
+        op.setIdOperateur(i);
+        op.setNom("oumayma");
+        op.setPrenom("afandvic");
+        op.setPassword("aaaaa");
 
+        Operateur ca=or.save(op);
+        Operateurs.add(ca);
+    }
+    assertEquals(10,Operateurs.size());
+}
 @Test
-@Order(5)
-
-public void TestDeleteOperateur() {
-	l.debug("Test méthode DeleteOperateur");
-	try {
-		operateurservice.deleteOperateurById((long) 6); 
-		
-		assertNull(os.getOperateurById((long) 6));
-		l.info(" operateur deleted succesfully");
-	} catch (Exception e) {
-		l.error("méthode Delete Operateur error :"+ e);
-	}
-	
+@Order(3)
+void TestdeleteAllOperateur() {
+    or.deleteAll();
+    assertEquals(0,or.findAll().spliterator().estimateSize());
 }
 @Test
 @Order(2)
-public void TestUpdateNomById() {
-	l.debug("Test méthode Modifier Nom d'un operateur by id");
-	try {
-		String nom= "devops";
+void TestretrieveOperateur() {
+    Mockito.when(or.findById(Mockito.anyLong())).thenReturn(Optional.of(o));
 
-		operateurservice.UpdateNomBy_Id(nom, (long) 3);
+    Mockito.when(or.findById(Mockito.anyLong())).thenReturn(Optional.of(o))
+    ;
+    Operateur op = os.retrieveOperateur(2L);
+    Assertions.assertNotNull(op);
 
-		Operateur o = operateurservice.getOperateurById((long) 3);
 
-		assertThat(o.getNom()).isEqualTo(nom);
-		l.info("nom operateur modified successfully!");
-		
-	} catch (Exception e) {
-		l.error(String.format("ERROR : %s ", e));
-	}
-}
-
-@Test
-@Order(3)
-public void TestUpdateOperateur() {
-	Mockito.when(or.save(o1)).thenReturn(o1);
-    assertNotNull(o1);
-    assertEquals(o1, os.updateOperateur(o1));
-
-    System.out.println("Operateur Updated Successfully !");
 }
 @Test
 @Order(4)
-public void TestRetrieveAllOperateurs() {
-		l.debug("Test méthode Retrieve Operateurs");
-		Mockito.when(or.findAll()).thenReturn(operateurlist);
-		List<Operateur> operateurlist = (List<Operateur>) operateurservice.retrieveAllOperateurs();
-		Assertions.assertNotNull(operateurlist);
-        l.info("Retrieve  All Operateurs done !!!");
-		}
-	}
+void TestgetAllOperateur(){
+    Iterable<Operateur> Operateurs = or.findAll();
+    Assertions.assertNotNull(Operateurs);
+}
 
-
-
-	
-
-
-	
-	
-	
-	
-
-	
-	
-	
-
-
-
+}	
