62424a1dc15083967a7800f1ca789b8cb08b33fd
ghada.setti <ghada.settii@gmail.com>
2022-11-13 17:29:47 +0100

    update pom

--- src/test/java/com/esprit/examen/services/CategorieProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/CategorieProduitServiceImplTest.java
@@ -19,20 +19,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.esprit.examen.entities.CategorieProduit;
 import com.esprit.examen.repositories.CategorieProduitRepository;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Service
-@Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
@@ -44,7 +38,6 @@ public class CategorieProduitServiceImplTest {
 	
 	@InjectMocks
 	CategorieProduitServiceImpl categorieProduitService;
-    private WebApplicationContext webApplicationContext;
 
     //Creation de categorieProduit pour le test
     CategorieProduit categorieProduit = new CategorieProduit("cat001","alimentaire");
@@ -58,8 +51,7 @@ public class CategorieProduitServiceImplTest {
 	@Test
 	@Order(1)
 	public void testAddCategorieProduit() {
-		
-		log.debug("Tester Ajout de categorieProduit");
+
 		when(catProduitRepository.save(ArgumentMatchers.any(CategorieProduit.class))).thenReturn(categorieProduit);
 				
 		// utiliser la methode dans le service		
@@ -72,7 +64,6 @@ public class CategorieProduitServiceImplTest {
     @Order(2)
     void testRetrieveCategorieProduit() {
 
-        log.debug("Tester retrive du CategorieProduit");
         when(catProduitRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(categorieProduit));
         
         Long CP1 = null;
@@ -87,7 +78,6 @@ public class CategorieProduitServiceImplTest {
 	@Test
     @Order(3)
     void testUpdateCategorieProduit() {
-        log.debug("Tester update du CategorieProduit");
         when(catProduitRepository.save(categorieProduit)).thenReturn(categorieProduit);
              
         // utiliser la methode dans le service
@@ -100,7 +90,6 @@ public class CategorieProduitServiceImplTest {
 	@Test
     @Order(4)
     void  testRetrieveAllCategorieProduits() throws Exception {
-        log.debug("Tester Retrive All CategorieProduits");
         when(catProduitRepository.findAll()).thenReturn(listCategorieProduits);
         List<CategorieProduit> listCategorieProduits = categorieProduitService.retrieveAllCategorieProduits();
         Assertions.assertNotNull(listCategorieProduits);
@@ -109,18 +98,14 @@ public class CategorieProduitServiceImplTest {
 	@Test
     @Order(5)
     void testDeleteCategorieProduit() {
-        log.debug("Test méthode Delete CategorieProduit");
-        try {
+        
            // when(catProduitRepository.findById(CategorieProduit.getIdCategorieProduit())).thenReturn(Optional.of(CategorieProduit));
 
             Long CP2 = null;
 			categorieProduitService.deleteCategorieProduit(CP2);
 
             assertNull(categorieProduitService.retrieveCategorieProduit(CP2));
-        } catch (Exception e) {
-            log.error("méthode Delete CategorieProduit error :" + e);
-        }
-
+       
 	}
 }
 
